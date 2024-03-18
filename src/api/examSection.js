c5c70cd1f81f170c0b625fc99611eb99a081d6c3
BenjaminS348 <coco.dragon002@outlook.com>
2024-03-18 07:22:13 +0100

    fix

--- src/api/examSection.js
+++ src/api/examSection.js
@@ -6,6 +6,17 @@ import {searchQuestions, searchSecureQuestions, getSecureQuestion} from '../cust
 import awsconfig from '../aws-exports'
 Amplify.configure(awsconfig)
 
+async function create(data) {
+  try {
+    let res = await API.graphql(graphqlOperation(createExamSection, 
+      {input: data}))
+    res = res?.data?.createExamSection
+
+    return Promise.resolve(res)
+  } catch(error) {
+    return Promise.reject(error)
+  }
+}
 
 async function get(id) {
   try {
