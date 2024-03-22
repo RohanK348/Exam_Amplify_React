805bcd9861078633c54f072ce228e6e5e1c68705
BenjaminS348 <coco.dragon002@outlook.com>
2024-03-22 06:44:26 +0100

    update

--- src/api/couponCandidate.js
+++ src/api/couponCandidate.js
@@ -16,6 +16,16 @@ async function create(data) {
   }
 }
 
+async function update(data) {
+  try {
+    await API.graphql(graphqlOperation(updateCouponCandidate, 
+      {input: data}))
+    
+    return Promise.resolve({message: 'success'})
+  } catch(error) {
+    return Promise.reject(error)
+  }
+}
 
 async function remove(id) {
   try {
