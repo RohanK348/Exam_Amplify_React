d809aa4e82a1b46b6fcd26dbebb1bc59266275d6
BenjaminS348 <coco.dragon002@outlook.com>
2024-03-26 06:42:07 +0100

    udpate attribute api

--- src/api/answerSection.js
+++ src/api/answerSection.js
@@ -5,6 +5,21 @@ import { updateAnswerSection } from '../graphql/mutations'
 import awsconfig from '../aws-exports'
 Amplify.configure(awsconfig)
 
+async function get(id) {
+  try {
+    let res = await API.graphql(graphqlOperation(getAnswerSection, {id}))
+    res = res?.data?.getAnswerSection
+    const answers = await Promise.all(res.answer.map( async(item) => {
+      const answer = await API.graphql(graphqlOperation(getAnswer, {id: item}))
+      return answer?.data?.getAnswer
+    }))
+    res.answers = answers
+    return Promise.resolve(res)
+  } catch(error) {
+    return Promise.reject(error)
+  }
+}
+
 async function update(data) {
   try {
     await API.graphql(graphqlOperation(updateAnswerSection, 
--- src/api/attribute.js
+++ src/api/attribute.js
@@ -5,16 +5,6 @@ import {getAttribute, listAttributes, searchAttributes} from '../graphql/queries
 import awsconfig from '../aws-exports'
 Amplify.configure(awsconfig)
 
-async function create(data) {
-  try {
-    await API.graphql(graphqlOperation(createAttribute, 
-      {input: data}))
-    
-    return Promise.resolve({message: 'success'})
-  } catch(error) {
-    return Promise.reject(error)
-  }
-}
 
 async function update(data) {
   try {
