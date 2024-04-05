2b421a848b0967bbd5f75788edaef724efb1de80
BenjaminS348 <coco.dragon002@outlook.com>
2024-04-05 21:23:44 +0200

    update paragraph

--- src/api/paragraph.js
+++ src/api/paragraph.js
@@ -18,16 +18,6 @@ async function create(data) {
   }
 }
 
-async function update(data) {
-  try {
-    await API.graphql(graphqlOperation(updateParagraph, 
-      {input: data}))
-    
-    return Promise.resolve({message: 'success'})
-  } catch(error) {
-    return Promise.reject(error)
-  }
-}
 
 async function remove(id) {
   try {
