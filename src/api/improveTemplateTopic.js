4b9e4fd63ab00fd2fe209c33e4591691a3ba64ee
BenjaminS348 <coco.dragon002@outlook.com>
2024-04-06 21:23:16 +0200

    fix

--- src/api/improveTemplateTopic.js
+++ src/api/improveTemplateTopic.js
@@ -5,6 +5,16 @@ import {getImproveTemplateTopic, listImproveTemplateTopics, searchImproveTemplat
 import awsconfig from '../aws-exports'
 Amplify.configure(awsconfig)
 
+async function create(data) {
+  try {
+    await API.graphql(graphqlOperation(createImproveTemplateTopic, 
+      {input: data}))
+    
+    return Promise.resolve({message: 'success'})
+  } catch(error) {
+    return Promise.reject(error)
+  }
+}
 
 async function update(data) {
   try {
