49ad191551d32805d5598b30946bf851a7bbefc8
BenjaminS348 <coco.dragon002@outlook.com>
2024-04-09 21:22:03 +0200

    fix

--- src/api/favoriteExam.js
+++ src/api/favoriteExam.js
@@ -20,6 +20,19 @@ async function add(userId, examId) {
   }
 }
 
+async function remove(userId, examId) {
+  try {
+    let res = await API.graphql(graphqlOperation(listFavoriteExams, {filter: {userID: {eq: userId}, examID: {eq: examId}}}))
+    res = res?.data?.listFavoriteExams?.items
+    if (res.length === 0)
+      return Promise.reject({message: 'do not exist'})
+    await API.graphql(graphqlOperation(deleteFavoriteExam, 
+      {input: {id: res[0].id}}))
+    return Promise.resolve(examId)
+  } catch(error) {
+    return Promise.reject(error)
+  }
+}
 
 async function getFilter(filter) {
   try {
